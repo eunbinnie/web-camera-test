90e46b8bde7e059714403eea0630aa8c951d33d5
eunbinnie <dmsqlsdl54@gmail.com>
2025-11-19 15:44:44 +0900

    feat: 카메라 연동 기능 추가

--- src/components/CameraTest.tsx
+++ src/components/CameraTest.tsx
@@ -1,16 +1,39 @@
+'use client';
+
+import { useRef } from 'react';
+
+const constraints = {
+  video: { width: 1280, height: 720, facingMode: 'user' },
+  audio: false,
+};
+
 const CameraTest = () => {
-  const handleStartCamera = constraints => {
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const handleStartCamera = async () => {
     let stream = null;
+
+    try {
+      stream = await navigator.mediaDevices.getUserMedia(constraints);
+      videoRef.current!.srcObject = stream;
+      videoRef.current!.onloadedmetadata = () => {
+        videoRef.current!.play();
+      };
+    } catch (error) {
+      console.error('Error starting camera:', error);
+      return;
+    }
   };
 
   return (
     <div>
       <button
         type="button"
+        onClick={handleStartCamera}
         className="text-4xl font-bold mb-4 cursor-pointer bg-gray-100 py-2 px-4 rounded-md"
       >
         Start Camera
       </button>
+      <video ref={videoRef} />
     </div>
   );
 };
